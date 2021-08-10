02c2adaf904c5d5460ec93c8d851d82a5c2d1153
Gabriel Rossi Lopes <rossi.gabriel@outlook.com>
2021-08-09 21:47:47 -0300

    pontos de interseções de diagonais com círculo

--- calc.js
+++ calc.js
@@ -7,88 +7,44 @@ const obtemCir = (centroX, centroY, raio) => {
     return f1;
 } 
 
-const obtemIntercoes = (pontoInicial, pontoFinal, passo) => {
-    const meio = (pontoFinal-pontoInicial)/2;
-    const eqCirc = obtemCir(meio, meio, meio);
+const resolveSistema = (ponto, limite) => {
+  const circulo = obtemCir(0, 0, 2);
 
-    let intercs = [];
-    for (i=pontoInicial+passo; i<= pontoFinal; i+=passo) {
-        let par = [];
-        par.push([i,0]);
-        par.push([0,i]);
-        let eqReta = new algebra.Equation(algebra.parse('y'), algebra.parse(`${obtemCoefReta(par)}*x + ${par[1][1]}`));
-
-        let novaEqYStr = eqCirc.toString().replaceAll('x', "(" + eqReta.solveFor('x').toString() + ")");
-
-        let novaEqY = algebra.parse(novaEqYStr);
-
-        let ys = novaEqY.solveFor('y')
-
-        
-        ys.forEach((e)=>{
-            let novaEqXStr = eqReta.toString().replaceAll('y', e);
-            let resp = algebra.parse(novaEqXStr).solveFor('x');
-
-            intercs.push([resp.valueOf(), e]);
-        })
-    }
-    
-    const supesq = [];
-    const supdir = [];
-    const infesq = [];
-    const infdir = [];
-    for (i=0; i < intercs.length; i++) {
-  
-      let ponto = intercs[i];
-      let x = ponto[0]-meio;
-      let y = ponto[1]-meio;
-  
-      if (x<0 && y>0) {
-        supesq.push(ponto);
-      }
-  
-      if (x>0 && y>0) {
-        supdir.push(ponto);
-      }
-  
-      if (x<0 && y<0) {
-        infesq.push(ponto);
-      }
-  
-      if (x>0 && y<0) {
-        infdir.push(ponto);
-      }
-  
-  
-    }
-  
-    pontos = []
-  
-    pontos.push(supdir);
-    pontos.push(infdir);
-    pontos.push(infesq);
-    pontos.push(supesq);
-  
-    pontos.forEach((e) => e.sort((a, b) => a[0] - b[0]));
-    console.log(pontos.flat().reverse());
-  
-    intercs = pontos.flat().reverse().flat();
-
-    return intercs;
+  let exprStr = circulo.toString();
+  exprStr = exprStr.replaceAll('x', `(${ponto[0]})`);
+  exprStr = exprStr.replaceAll('y', `(${ponto[1]})`);
+  let eq = algebra.parse(`${exprStr} + r`);
+  return eq.solveFor('r').valueOf();
 }
 
-const xMax=10;
-const xMin=0;
-const yMax=10;
-const yMin=0;
+const obtemPontos = (precisao, limite) => {
 
-const passo=2;
+  const pontosRetas = [];
+  for (linha=0;linha < limite - precisao; linha++) {
+      for (coluna=0; coluna < limite - precisao; coluna++) {
 
-// console.log('Interseções:');
-const intercs = obtemIntercoes(0,10,2);
-// console.log(intercs);
+        let pontoA = [coluna*precisao - limite/2, linha*precisao - limite/2];
+        let pontoB = [coluna*precisao + precisao - limite/2, linha*precisao - limite/2];
+        let pontoC = [coluna*precisao - limite/2, linha*precisao + precisao - limite/2];
+        let pontoD = [coluna*precisao + precisao - limite/2, linha*precisao + precisao - limite/2];
+
+        
+
+        let paresOpostos=[[pontoA, pontoD], [pontoB, pontoC]];
+
+        paresOpostos.forEach((pontos)=>{
+          if (resolveSistema(pontos[0], limite)*resolveSistema(pontos[1], limite) < 0) {
+            pontosRetas.push(pontos);
+          }
+        })
+
+      }
+  }
+  console.log(pontosRetas)
+}
 
 
+obtemPontos(2, 4);
 
 
 
